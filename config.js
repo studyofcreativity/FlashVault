44d69cc4275670e6e245cb359ad2ce6a6915e1c5
studyofcreativity <study.of.creativity@gmail.com>
2026-09-04 19:09:43 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,7 @@
 window.FLASHVAULT_CONFIG = {
   SUPABASE_URL: 'https://artpsiwpbmjomielhdbi.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_ZsWkGEsEFiLaAW0LM4kBig_aXZ1nkVV',
+  ADMIN_USER_ID: '77145dbb-f0cd-4483-86f3-dcfff6b16ae9',
   // CAMBIA SOLO ESTE VALOR. La seguridad real la hacen Supabase Auth + RLS.
   EXTRA_ADMIN_KEY: 'Flash.SupaBase.Kiltio.Subir.201010'
 };
